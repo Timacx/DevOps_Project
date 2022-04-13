08d445f7c997c489a1c6fd52e90c1b318bf1ee87
Yahya BOULOUH <boulouhyahya@outlook.fr>
2022-04-13 08:39:54 +0200

    Ajout des fichhiers

---
+++ Project_DevOps/src/main/java/demo/DataFrame.java
@@ -0,0 +1,24 @@
+package demo;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class DataFrame 
+{
+    // Constructeur à partir d'un tableau
+    DataFrame( ArrayList<Object[]> strucList ){
+
+    }
+
+    // Constructeur à partir d'un fichier CSV
+    DataFrame( String fileName ){
+
+    }
+    public static void main( String[] args )
+    {
+        System.out.println("DevOps Project");
+    }
+}
---
+++ Project_DevOps/src/test/java/demo/DataFrameTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class DataFrameTest 
+    extends TestCase
+{
+}
